effe40f4b51618d88a42270fdf702db6511af318
Malte <malte.dostal@gmail.com>
2021-02-27 10:54:19 +0100

    greatly improve PieceRenderer's init process and javaDoc

--- src/main/java/de/sanj0/jess/PieceRenderer.java
+++ src/main/java/de/sanj0/jess/PieceRenderer.java
@@ -7,73 +7,112 @@ import de.edgelord.saltyengine.transform.Transform;
 import de.edgelord.saltyengine.utils.ImageLoader;
 import de.edgelord.saltyengine.utils.ImageUtils;
 
-import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
 
 import static de.sanj0.jess.Piece.*;
 
 /**
- * Renders pieces
- * pngs from Sebastian Lague on github, copyright notice in root/src/main/resources/img/copyright.txt
+ * Renders pieces the png is from Sebastian Lague on github, copyright notice in
+ * /src/main/resources/img/copyright.txt
  */
 public class PieceRenderer {
+    /**
+     * The spritesheet image that contains all the pieces.
+     */
     private static final SaltyImage sheet = ImageLoader.getOrLoadImage("spritesheet", "img/pieces.png");
-    
-    private static SaltyImage PAWN_LIGHT;
-    private static SaltyImage PAWN_DARK;
-    private static SaltyImage KNIGHT_LIGHT;
-    private static SaltyImage KNIGHT_DARK;
-    private static SaltyImage BISHOP_LIGHT;
-    private static SaltyImage BISHOP_DARK;
-    private static SaltyImage ROOK_LIGHT;
-    private static SaltyImage ROOK_DARK;
-    private static SaltyImage QUEEN_LIGHT;
-    private static SaltyImage QUEEN_DARK;
-    private static SaltyImage KING_LIGHT;
-    private static SaltyImage KING_DARK;
-    
-    private static Map<Byte, SaltyImage> imageMap = new HashMap<>();
+    /**
+     * How many rows of sprites are there in the spritesheet?
+     */
+    private static final int SPRITE_ROWS = 2;
+    /**
+     * How many sprites PER row are there in the spritesheet?
+     */
+    private static final int SPRITES_PER_ROW = 6;
+    /**
+     * The order of pieces in the spritesheet, from left to right and top to
+     * bottom.
+     */
+    private static final byte[] SPRITES_KEY = {
+            LIGHT | KING,
+            LIGHT | QUEEN,
+            LIGHT | BISHOP,
+            LIGHT | KNIGHT,
+            LIGHT | ROOK,
+            LIGHT | PAWN,
+            // next row
+            DARK | KING,
+            DARK | QUEEN,
+            DARK | BISHOP,
+            DARK | KNIGHT,
+            DARK | ROOK,
+            DARK | PAWN
+    };
+    /**
+     * The map used to store and later query the piece images
+     */
+    private static final Map<Byte, SaltyImage> imageMap = new HashMap<>();
 
+    /**
+     * Empty constructor to avoid the generation of the default public one.
+     */
+    private PieceRenderer() {
+    }
+
+    /**
+     * Draws the given piece to the given {@link SaltyGraphics graphics} by
+     * querying an image from the {@link #imageMap map} and drawing it at the
+     * position of the given {@link Transform}, ignoring its size and instead
+     * using the default size of the queried image, which is equal to {@link
+     * BoardRenderer#squareSize}.
+     *
+     * @param g         the graphics to render to
+     * @param piece     the piece to render
+     * @param transform the position of the piece to be rendered at
+     */
     public static void drawPiece(final SaltyGraphics g, final byte piece, final Transform transform) {
         if (piece == NONE) {
             return;
         }
         g.drawImage(imageMap.get(piece), transform.getPosition());
     }
-    
+
+    /**
+     * Initializes the PieceRenderer by filling the {@link #imageMap image map}
+     * with all the pieces from the {@link #sheet sprite sheet}.
+     */
     public static void init() {
         final Spritesheet spritesheet = new Spritesheet(sheet, 333.33f, 334f);
-        KING_LIGHT = getPieceImage(spritesheet, 0, 0);
-        QUEEN_LIGHT = getPieceImage(spritesheet, 1, 0);
-        BISHOP_LIGHT = getPieceImage(spritesheet, 2, 0);
-        KNIGHT_LIGHT = getPieceImage(spritesheet, 3, 0);
-        ROOK_LIGHT = getPieceImage(spritesheet, 4, 0);
-        PAWN_LIGHT = getPieceImage(spritesheet, 5, 0);
+        final float width = BoardRenderer.squareSize.getWidth();
+        final float height = BoardRenderer.squareSize.getHeight();
 
-        KING_DARK = getPieceImage(spritesheet, 0, 1);
-        QUEEN_DARK = getPieceImage(spritesheet, 1, 1);
-        BISHOP_DARK = getPieceImage(spritesheet, 2, 1);
-        KNIGHT_DARK = getPieceImage(spritesheet, 3, 1);
-        ROOK_DARK = getPieceImage(spritesheet, 4, 1);
-        PAWN_DARK = getPieceImage(spritesheet, 5, 1);
-        
-        imageMap.put(get(PAWN, LIGHT), PAWN_LIGHT);
-        imageMap.put(get(KNIGHT, LIGHT), KNIGHT_LIGHT);
-        imageMap.put(get(BISHOP, LIGHT), BISHOP_LIGHT);
-        imageMap.put(get(ROOK, LIGHT), ROOK_LIGHT);
-        imageMap.put(get(QUEEN, LIGHT), QUEEN_LIGHT);
-        imageMap.put(get(KING, LIGHT), KING_LIGHT);
-
-        imageMap.put(get(PAWN, DARK), PAWN_DARK);
-        imageMap.put(get(KNIGHT, DARK), KNIGHT_DARK);
-        imageMap.put(get(BISHOP, DARK), BISHOP_DARK);
-        imageMap.put(get(ROOK, DARK), ROOK_DARK);
-        imageMap.put(get(QUEEN, DARK), QUEEN_DARK);
-        imageMap.put(get(KING, DARK), KING_DARK);
+        for (int y = 0; y < SPRITE_ROWS; y++) {
+            for (int x = 0; x < SPRITES_PER_ROW; x++) {
+                imageMap.put(SPRITES_KEY[y * SPRITES_PER_ROW + x],
+                        getPieceImage(spritesheet, x, y, width, height));
+            }
+        }
     }
 
-    private static SaltyImage getPieceImage(final Spritesheet spritesheet, final int x, final int y) {
-        return ImageUtils.resize(spritesheet.getFrame(x, y).getImage(), 100, 100);
+    /**
+     * Gets the sprite specified by its x and y coordinates from the given
+     * {@link Spritesheet}, scales it to the given width and height and returns
+     * it.
+     *
+     * @param spritesheet the spritesheet
+     * @param x           the x coordinate of the desired sprite
+     * @param y           the y coordinate of the desired sprite
+     * @param width       the width for the image to be scaled to
+     * @param height      the height for the image to be scaled to
+     *
+     * @return the sprite at the given coordinates in the given SpriteSheet
+     * sclaled to the given width and height
+     * @see ImageUtils#resize(SaltyImage, float, float)
+     * @see Spritesheet#getFrame(int, int)
+     */
+    private static SaltyImage getPieceImage(final Spritesheet spritesheet,
+                                            final int x, final int y,
+                                            final float width, final float height) {
+        return ImageUtils.resize(spritesheet.getFrame(x, y).getImage(), width, height);
     }
 }
