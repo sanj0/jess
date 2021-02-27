f2247dd22f2af7f942b6a76706e0efad3172f656
Malte <malte.dostal@gmail.com>
2021-02-27 11:28:29 +0100

    fix a series of potential bugs by checking for mouse BUTTON1 for moves

--- src/main/java/de/sanj0/jess/ChessScene.java
+++ src/main/java/de/sanj0/jess/ChessScene.java
@@ -23,24 +23,28 @@ public class ChessScene extends Scene {
         Input.addMouseInputHandler(new MouseInputAdapter() {
             @Override
             public void mousePressed(final MouseEvent e) {
-                final MoveState moveState = boardRenderer.getMoveState();
-                final int draggedPieceIndex = BoardRenderer.indexOfPosition(e.getX(), e.getY());
-                moveState.setDraggedPieceIndex(draggedPieceIndex);
-                moveState.setDraggedPiece(board.getPosition()[draggedPieceIndex]);
+                if (e.getButton() == MouseEvent.BUTTON1) {
+                    final MoveState moveState = boardRenderer.getMoveState();
+                    final int draggedPieceIndex = BoardRenderer.indexOfPosition(e.getX(), e.getY());
+                    moveState.setDraggedPieceIndex(draggedPieceIndex);
+                    moveState.setDraggedPiece(board.getPosition()[draggedPieceIndex]);
+                }
             }
 
             @Override
             public void mouseReleased(final MouseEvent e) {
-                final MoveState moveState = boardRenderer.getMoveState();
-                // check if square is legal and make the move or
-                // move back to origin
-                final int position = BoardRenderer.indexOfPosition(e.getX(), e.getY());
-                if (position != moveState.getDraggedPieceIndex()) {
-                    board.getPosition()[BoardRenderer.indexOfPosition(e.getX(), e.getY())] = moveState.getDraggedPiece();
-                    board.getPosition()[moveState.getDraggedPieceIndex()] = Piece.NONE;
+                if (e.getButton() == MouseEvent.BUTTON1) {
+                    final MoveState moveState = boardRenderer.getMoveState();
+                    // check if square is legal and make the move or
+                    // move back to origin
+                    final int position = BoardRenderer.indexOfPosition(e.getX(), e.getY());
+                    if (position != moveState.getDraggedPieceIndex()) {
+                        board.getPosition()[BoardRenderer.indexOfPosition(e.getX(), e.getY())] = moveState.getDraggedPiece();
+                        board.getPosition()[moveState.getDraggedPieceIndex()] = Piece.NONE;
+                    }
+                    moveState.setDraggedPieceIndex(-1);
+                    moveState.setDraggedPiece(Piece.NONE);
                 }
-                moveState.setDraggedPieceIndex(-1);
-                moveState.setDraggedPiece(Piece.NONE);
             }
 
             @Override
