2664b92d08c088510c1e9e1eadff1d211ea8be24
Malte <malte.dostal@gmail.com>
2021-03-03 11:46:58 +0100

    add STARTING_FEN to Main and cli arg evaluation

--- src/main/java/de/sanj0/jess/Main.java
+++ src/main/java/de/sanj0/jess/Main.java
@@ -5,10 +5,22 @@ import de.edgelord.saltyengine.core.GameConfig;
 
 public class Main extends Game {
 
+    /**
+     * The FEN notation of the position,
+     * the sandbox started with.
+     * <br>This is either {@link BoardPositions#STARTING_POSITION}
+     * or a command line argument.
+     */
+    public static String STARTING_FEN = BoardPositions.STARTING_POSITION;
+
     public static void main(String[] args) {
+        if (args.length == 1) {
+            STARTING_FEN = args[0];
+        }
+
         init(GameConfig.config(800, 800, "Jess", 5));
         PieceRenderer.init();
-        start(30, new ChessScene(new Board(BoardPositions.parseFEN(BoardPositions.STARTING_POSITION))));
+        start(30, new ChessScene(new Board(BoardPositions.parseFEN(STARTING_FEN))));
         Game.setDrawFPS(false);
     }
 }
